aa18b4e1672975862c936ee653abca08ebdcc0d0
Luis <ljmora1@utpl.edu.ec>
2022-12-21 10:50:21 -0500

    Clase 03

--- Ejemplosc3/src/paquete04/Ejemplo2.java
+++ Ejemplosc3/src/paquete04/Ejemplo2.java
@@ -7,7 +7,7 @@ package paquete04;
 
 /**
  *
- * @author reroes
+ * @author Luis Mora A.
  */
 public class Ejemplo2 {
 
@@ -22,7 +22,27 @@ public class Ejemplo2 {
             1   4   3
             36  64  9
         */
+        double [][] resultado = new double[2][3];
+        int fila;
+        int columna;
+        double operacion;
         
+        for(fila = 0; fila < dato1.length; fila++){
+            for(columna = 0; columna < dato1[fila].length; columna++){
+                if(dato1[fila][columna] % 2 == 0){
+                    operacion = Math.pow(dato1[fila][columna],2);
+                    resultado[fila][columna] = operacion;
+                }else{
+                    resultado[fila][columna] = dato1[fila][columna];
+                }
+            }
+        }
+        for(fila = 0; fila < dato1.length; fila++){
+            for(columna = 0; columna < dato1[fila].length; columna++){
+                System.out.printf("%.0f\t", resultado[fila][columna]);
+            }
+            System.out.println();
+        }
     }
     
 }
